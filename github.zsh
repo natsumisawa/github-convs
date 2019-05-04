0c15f517f59a1179207396f0029604e119db65a9
natsumisawa <natsumi.sawa@bizreach.co.jp>
2019-05-04 20:43:04 +0900

    🐛fix bug

--- github.zsh
+++ github.zsh
@@ -62,6 +62,7 @@ git-add-cmt(){
     git commit -m $EMOJI$MSG && \
     echo ":::::::::::::::: commit\U1F35D ::::::::::::::::"
 }
+
 # add each part and commit
 git-add-prt-cmt(){
   git add -p && \
